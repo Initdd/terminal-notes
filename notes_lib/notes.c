a86162aff82e3b05b58572ecaa99201203f6983e
Initdd <a50468@alunos.isel.pt>
2024-02-04 23:12:33 +0000

    updated notes.c to correct a last_id incrementation, free of some data and set that pointer NULL

--- notes_lib/notes.c
+++ notes_lib/notes.c
@@ -25,6 +25,8 @@ Note *notes_create(char *data, int prt) {
 	strcpy(note->data, data);
 	// set the priority
 	note->prt = prt;
+	// set the id
+	note->id = 0;
 	// return the note
 	return note;
 }
@@ -46,6 +48,7 @@ void notes_update(Note *note, char *data, int prt) {
 	if (data != NULL) {
 		// free the old data
 		free(note->data);
+		note->data = NULL;
 		// allocate memory for the new data
 		note->data = (char *)malloc(sizeof(char) * (strlen(data) + 1));
 		// copy the data to the note
@@ -60,8 +63,10 @@ void notes_update(Note *note, char *data, int prt) {
 void notes_delete(Note *note) {
 	// free the note data
 	free(note->data);
+	note->data = NULL;
 	// free the note
 	free(note);
+	note = NULL;
 }
 
 // Notes List Implementation
@@ -96,6 +101,7 @@ char *notes_list_read(NoteList *list) {
         if (note_str == NULL) {
             // handle notes_read failure
             free(str);
+			str = NULL;
             return NULL;
         }
         // reallocate memory for the string
@@ -104,7 +110,9 @@ char *notes_list_read(NoteList *list) {
         if (temp == NULL) {
             // handle realloc failure
             free(str);
+			str = NULL;
             free(note_str);
+			note_str = NULL;
             return NULL;
         }
         str = temp;
@@ -114,6 +122,7 @@ char *notes_list_read(NoteList *list) {
         strcat(str, note_str);
         // free the note string
         free(note_str);
+		note_str = NULL;
     }
     // return the string
     return str;
@@ -146,7 +155,9 @@ void notes_list_add(NoteList *list, Note *note) {
 	}
 	// add the note to the list
 	list->list[list->size] = note;
-	list->list[list->size]->id = list->last_id++;
+	// set the note id
+	list->list[list->size]->id = list->last_id;
+	list->last_id++;
 	list->size++;
 }
 
@@ -190,11 +201,15 @@ void notes_list_delete(NoteList *list) {
 	// delete all the notes
 	for (int i = 0; i < list->size; i++) {
 		notes_delete(list->list[i]);
+		// set the note to NULL
+		list->list[i] = NULL;
 	}
 	// free the list data
 	free(list->list);
+	list->list = NULL;
 	// free the list
 	free(list);
+	list = NULL;
 }
 
 void notes_list_foreach(NoteList *list, void (*func)(Note *)) {
@@ -211,7 +226,7 @@ void notes_list_save(NoteList *list, char *path, char delimiter) {
 	FILE *file = fopen(path, "w");
 	// write the list to the file
 	for (int i = 0; i < list->size; i++) {
-		fprintf(file, "%d%c%d%c%s\n", list->list[i]->id, delimiter, list->list[i]->prt, delimiter, list->list[i]->data);
+		fprintf(file, "%d%c%s\n", list->list[i]->prt, delimiter, list->list[i]->data);
 	}
 	// close the file
 	fclose(file);
@@ -224,13 +239,12 @@ NoteList *notes_list_load(char *path, char delimiter) {
 	// create a new list
 	NoteList *list = notes_list_create();
 	// read the file
-	int id, prt;
+	int prt;
 	char data[255];
-	char *format = "%d%c%d%c%[^\n]";
-	while (fscanf(file, format, &id, &delimiter, &prt, &delimiter, data) != EOF) {
+	char *format = "%d%c%[^\n]";
+	while (fscanf(file, format, &prt, &delimiter, data) != EOF) {
 		// add the note to the list
 		notes_list_add(list, notes_create(data, prt));
-		list->list[list->size - 1]->id = id;
 	}
 	// close the file
 	fclose(file);

92c473576b32d7552052f49dc2f8aaf937937f73
Initdd <a50468@alunos.isel.pt>
2024-03-04 19:32:04 +0000

    Add group_read function to read a group into a string

--- group/group.h
+++ group/group.h
@@ -37,16 +37,14 @@ typedef struct group {
 
 // Create a new group with a specified name
 Group *group_create(const char *name);
-
+// Read a group into a string
+char *group_read(Group *group, char delimiter);
 // Add a item to a group
 void group_add_item(Group *group, void *item);
-
 // Remove a item from a group
 void group_remove_item(Group *group, void (*deleter)(void *item), void *item, int (*cmp)(void *a, void *b));
-
 // Update a group name
 void group_update_name(Group *group, const char *name);
-
 // Delete a group
 void group_delete(Group *group, void (*deleter)(void *item));
 
